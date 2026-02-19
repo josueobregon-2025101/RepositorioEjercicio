b1d1fc0dcd9c91c17e70ebf74e65170f1126225b
josueobregon <jobregon-2025101@kinal.edu.gt>
2026-02-19 14:55:27 -0600

    feat: Se agregaron las entidades a las base de datos, sin procedimientos almacenados

--- DBpracticasEstudiantes_in5cm.sql
+++ DBpracticasEstudiantes_in5cm.sql
@@ -1,4 +1,158 @@
 drop database if exists DBpracticasEstudiantes_in5cm;
 create database DBpracticasEstudiantes_in5cm;
 use DBpracticasEstudiantes_in5cm;
+
+drop database if exists DBpracticasEstudiantes_in5cm;
+create database DBpracticasEstudiantes_in5cm;
+use DBpracticasEstudiantes_in5cm;
+
+create table Login(
+	id_login int auto_increment not null,
+    correo_login varchar(50) not null,
+    usuario_login varchar(30) not null,
+    contrasena_login varchar(20) not null,
+    roles varchar(45) not null,
+    primary key (id_login)
+);
+ 
+create table Empresa(
+	id_empresa int auto_increment not null,
+    nombre_empresa varchar(45),
+    tipo_empresa varchar(45) not null,
+    tamano_empresa varchar(20),
+    telefono_empresa varchar(15),
+    correo_empresa varchar(50),
+    direccion_empresa varchar(45),
+    horario_empresa varchar(45),
+    descripcion text,
+    id_login int not null,
+    primary key (id_empresa),
+    constraint FK_empresa_login foreign key (id_login)
+	references Login(id_login) on delete cascade
+);
+ 
+create table Administradores(
+	id_administradores int auto_increment not null,
+    nombre_administradores varchar(45) not null,
+    apellido_administradores varchar(45) not null,
+    estado_administradores varchar(45) not null,
+    id_login int not null,
+    primary key (id_administradores),
+    constraint FK_admin_login foreign key (id_login) 
+    references Login(id_login) on delete cascade
+);
+
+
+create table RepresentanteEmpresa (
+	id_representantempresa int auto_increment not null,
+    id_empresa int,
+    nombres varchar(45),
+    apellidos varchar(45),
+    cargo varchar(45),
+    telefono int,
+    extension varchar(45),
+    estado varchar(45),
+    fecha_registro date,
+    correo varchar(45),
+    primary key(id_representantempresa),
+    constraint fk_empresa foreign key (id_empresa)
+    references Empresa(id_empresa) on delete cascade
+);
+create table Instituciones(
+	id_institucion int auto_increment not null,
+    nombre_institucion varchar(50),
+    correo_institucion varchar(100),
+    direccion_institucion varchar(100),
+    numero_telefono varchar(20),
+    primary key (id_institucion)
+);
+
+create table Estudiantes (
+	id_estudiante int auto_increment not null,
+    id_institucion int,
+    id_login int,
+    nombre varchar(30) not null,
+    apellido varchar(30) not null,
+    telefono int,
+    grado varchar(30),
+    carrera varchar(40),
+    correo varchar(50),
+    nombreinstitucion varchar(40),
+    tutortel int,
+    edad int,
+    primary key(id_estudiante),
+    constraint fk_institucion foreign key (id_institucion)
+	references Instituciones(id_institucion) on delete cascade,
+    constraint fk_login foreign key (id_login) 
+    references Login(id_login) on delete cascade
+);
+
+create table RepresentanteInstitucion(
+	id_representante_institucion int auto_increment not null,
+    nombre_representante_institucion varchar(50),
+    apellido_representante_institucion varchar(50),
+    numero_telefono varchar(20),
+    correo_representante_institucion varchar(100),
+    id_institucion int,
+    id_estudiante int ,
+    primary key(id_representante_institucion),
+    constraint fk_id_institucion foreign key (id_institucion)
+	references Instituciones(id_institucion) on delete cascade,
+    constraint fk_estudiante foreign key (id_estudiante)
+	references Estudiantes(id_estudiante) on delete cascade
+);
+
+create table Practicas(
+	id_practica int auto_increment not null,
+    id_empresa int,
+    titulo varchar(60) not null,
+    tiempo_practica datetime not null,
+    tipo_practica varchar(32) not null,
+    carrera_practica varchar(60) not null,
+    vigencia varchar(32) not null,
+    disponibilidad varchar(60) not null,
+    horario datetime not null,
+    primary key(id_practica),
+    constraint FK_Practicas_empresa foreign key(id_empresa)
+    references Empresa(id_empresa) on delete cascade
+);
+ 
+create table Postulaciones(
+	id_postulación int auto_increment not null,
+    id_practica int,
+    titulo varchar(60) not null,
+    descripcion varchar(128) not null,
+    fecha_post varchar(32) not null,
+    estado varchar(32) not null,
+    primary key(id_postulación),
+    constraint FK_Postulaciones_practica foreign key (id_practica)
+    references Practicas(id_practica) on delete cascade
+);
+
+Create table Documentos(
+	id_documento int not null auto_increment,
+    id_estudiante int,
+    id_empresa int,
+    tipoDoc enum("Carta de Solicitud de Práctica","Carta de Presentación de la Institución",
+		"Curriculum Vitae (CV)","Fotocopia de DPI o CUI","Constancia de Estudios","Pensum de la Carrera",
+		"Certificación de Notas","Constancia de Seguro Estudiantil","Fotografías tamaño cédula",
+		"Informe Final de Práctica"),
+    nombreArchivo varchar(50),
+    urlArchivo varchar(2048),
+    fechaSubida datetime,
+    primary key (id_documento)
+);
+
+Create table Contrato(
+	id_contrato int not null auto_increment,
+    id_postulacion int,
+    id_empresa int,
+    id_estudiante int,
+    id_documento int,
+    fechaInicio date,
+    fechaFin date,
+    objetivos varchar(80),
+    primary key (id_contrato)
+);
+-- ////////////////////////----Procedimientos Almacenados----///////////////////////
  
