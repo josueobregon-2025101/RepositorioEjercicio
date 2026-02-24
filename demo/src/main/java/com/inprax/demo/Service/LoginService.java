82b6de9d18d2a299a44b7e1bb232de5123b4e277
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-24 10:30:36 -0600

    Feat: Se agrego la logica de LoginService

--- demo/src/main/java/com/inprax/demo/Service/LoginService.java
+++ demo/src/main/java/com/inprax/demo/Service/LoginService.java
@@ -1,4 +1,10 @@
 package com.inprax.demo.Service;
 
+import org.springframework.stereotype.Service;
+import com.inprax.demo.Entity.Login;
+
+@Service
 public interface LoginService {
+
+    Login validarLogin(String correoLogin, String usuarioLogin, String contrasenaLogin, String roles);
 }
