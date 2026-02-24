7bc3a6948ccea89814683f15d70d8d4649fd6043
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-24 10:23:15 -0600

    feat: Se creo la clase InstitucionRepository con exito.

--- demo/src/main/java/com/inprax/demo/Repository/InstitucionRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/InstitucionRepository.java
@@ -1,4 +1,18 @@
 package com.inprax.demo.Repository;
 
-public interface InstitucionRepository {
+import com.marcosmontenegro.Entity.Institucion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstitucionRepository extends JpaRepository<Institucion, Integer> {
+
+
+    boolean existsByNombreAndCorreoAndDireccionAndTelefono(
+            String nombre,
+            String correo,
+            String direccion,
+            String telefono
+    );
 }
+
