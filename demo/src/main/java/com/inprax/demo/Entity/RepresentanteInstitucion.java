ec16012c693bbaa4a2d056d2578ee0e61345025a
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-24 18:51:52 -0600

    feat: Se creo la clase entity RepresentanteInstitucion

---
+++ demo/src/main/java/com/inprax/demo/Entity/RepresentanteInstitucion.java
@@ -0,0 +1,114 @@
+package com.inprax.demo.Entity;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "RepresentanteInstitucion")
+public class RepresentanteInstitucion {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "id_representante_institucion")
+    private Integer id;
+
+    @NotBlank(message = "El nombre del representante es obligatorio")
+    @Column(name = "nombre_representante_institucion")
+    private String nombre;
+
+    @NotBlank(message = "El apellido del representante es obligatorio")
+    @Column(name = "apellido_representante_institucion")
+    private String apellido;
+
+    @NotBlank(message = "El numero de telefono es obligatorio")
+    @Column(name = "numero_telefono")
+    private String telefono;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Formato de correo no valido")
+    private String correo;
+
+    @OneToMany(mappedBy = "representanteInstitucion", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore 
+    private List<Estudiantes> estudiantes;
+
+    @NotNull(message = "La institución es obligatoria")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_institucion")
+    private Institucion institucion;
+
+// GETTERS AND SETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public List<Estudiantes> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(List<Estudiantes> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    public Institucion getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(Institucion institucion) {
+        this.institucion = institucion;
+    }
+
+
+}
