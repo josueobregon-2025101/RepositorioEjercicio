e6bce414270bd714f16fed63ddcc1130f0bcd97c
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-25 08:27:56 -0600

    Feat: Se agrego la logica de EmpresaRepository

--- demo/src/main/java/com/inprax/demo/Repository/EmpresaRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/EmpresaRepository.java
@@ -1,4 +1,9 @@
 package com.inprax.demo.Repository;
 
-public interface EmpresaRepository {
+import com.inprax.demo.Entity.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
 }
