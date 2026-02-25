5f259c63da7e1676d42035abc371d58ac8accb01
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-25 08:10:33 -0600

    Feat: Se agrego la logica de EmpresaService

--- demo/src/main/java/com/inprax/demo/Service/EmpresaService.java
+++ demo/src/main/java/com/inprax/demo/Service/EmpresaService.java
@@ -1,4 +1,20 @@
 package com.inprax.demo.Service;
 
+import com.inprax.demo.Entity.Empresa;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface EmpresaService {
+
+    List<Empresa> getAllEmpresas();
+
+    Empresa getEmpresaById(Integer id);
+
+    Empresa saveEmpresa(Empresa empresa);
+
+    Empresa updateEmpresa(Integer id, Empresa empresa);
+
+    void deleteEmpresa(Integer id);
 }
