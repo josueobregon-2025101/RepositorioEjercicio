7e551d3b8b488c89a19a4531143d7c3ec495908d
Mateo <mortiz-2024204@kinal.edu.gt>
2026-02-25 08:52:31 -0600

    Feat: Se agrego la logica de AdministradoresRepository

--- demo/src/main/java/com/inprax/demo/Repository/AdministradoresRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/AdministradoresRepository.java
@@ -1,4 +1,9 @@
 package com.inprax.demo.Repository;
 
-public interface AdministradoresRepository {
+import com.inprax.demo.Entity.Administradores;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdministradoresRepository extends JpaRepository<Administradores, Integer> {
 }
