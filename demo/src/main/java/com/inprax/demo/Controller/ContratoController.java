d9c1ce631f56f3b556d1b4a0c478af723eb1db28
josueobregon <jobregon-2025101@kinal.edu.gt>
2026-02-24 20:18:05 -0600

    Feat: Se completo el Controlador de Contratos

---
+++ demo/src/main/java/com/inprax/demo/Controller/ContratoController.java
@@ -0,0 +1,70 @@
+package com.inprax.demo.Controller;
+
+import com.inprax.demo.Entity.Contrato;
+import com.inprax.demo.Service.ContratoService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Contrato")
+public class ContratoController {
+    public final ContratoService contratoService;
+    public ContratoController(ContratoService contratoService) {
+        this.contratoService = contratoService;
+    }
+
+    @GetMapping
+    public List<Contrato> GetAllContratos(){
+        return contratoService.getAllContratos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> GetContratoById(@PathVariable @Valid Integer id){
+        try {
+            Contrato contrato = contratoService.getContratoById(id);
+            return ResponseEntity.ok().body(contrato);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createContrato(@RequestBody @Valid Contrato contrato){
+        try{
+            Contrato creado = contratoService.saveContrato(contrato);
+            return ResponseEntity.ok().body(creado);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateContrato(@PathVariable Integer id,@RequestBody @Valid Contrato contrato){
+        try {
+            Contrato existente = contratoService.getContratoById(id);
+            if(existente!= null){
+                Contrato update = contratoService.updateContrato(id,contrato);
+                return ResponseEntity.ok().body(update);
+            }else {
+                return ResponseEntity.notFound().build();
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteContratoById(@PathVariable @Valid Integer id){
+        Contrato contrato = contratoService.getContratoById(id);
+        if(contrato != null){
+            contratoService.deleteContratoById(id);
+            return ResponseEntity.ok().body("Se elimino el contrato");
+        }else{
+            return ResponseEntity.status(404).body("No existe el contrato");
+        }
+    }
+}
