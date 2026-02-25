4095a15b82a53c9c1052d2957c7cd0409cb26620
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-02-24 18:48:07 -0600

    feat: Se añadieron y cambiaron funcionalidades y funciones en la clase EstudiantesRepository

--- demo/src/main/java/com/inprax/demo/Repository/EstudiantesRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/EstudiantesRepository.java
@@ -1,10 +1,23 @@
 package com.inprax.demo.Repository;
 
 import com.inprax.demo.Entity.Estudiantes;
+import com.inprax.demo.Entity.Institucion;
+import com.inprax.demo.Entity.RepresentanteInstitucion;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EstudiantesRepository extends JpaRepository<Estudiantes, Integer> {
-
+    boolean existsByNombreAndApellidoAndCorreoAndTelefonoAndGradoAndCarreraAndEdadAndInstitucionAndRepresentanteInstitucion(
+            String nombre, 
+            String apellido, 
+            String correo, 
+            Integer telefono, 
+            String grado, 
+            String carrera, 
+            Integer edad, 
+            Institucion institucion, 
+            RepresentanteInstitucion representanteInstitucion
+    );
 }
