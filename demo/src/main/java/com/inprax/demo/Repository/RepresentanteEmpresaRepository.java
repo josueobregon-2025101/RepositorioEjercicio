04c529d6bd643b1478dc9444bb421bd91c1c41c2
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-02-25 11:19:53 -0600

    feat:Se ha creado un nuevo repository de la entidad EmpresaRepository

---
+++ demo/src/main/java/com/inprax/demo/Repository/RepresentanteEmpresaRepository.java
@@ -0,0 +1,7 @@
+package com.inprax.demo.Repository;
+
+import com.inprax.demo.Entity.RepresentanteEmpresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepresentanteEmpresaRepository extends JpaRepository<RepresentanteEmpresa, Integer> {
+}
