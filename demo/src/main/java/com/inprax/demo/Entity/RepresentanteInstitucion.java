c956068909925699065216c7add06fa937b45590
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-03-03 11:06:46 -0600

    fix: Se corrigio un error mas en la clase Entity RepresentanteInstitucion.java

--- demo/src/main/java/com/inprax/demo/Entity/RepresentanteInstitucion.java
+++ demo/src/main/java/com/inprax/demo/Entity/RepresentanteInstitucion.java
@@ -4,16 +4,7 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,7 +14,7 @@ import jakarta.validation.constraints.NotNull;
 public class RepresentanteInstitucion {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_representante_institucion")
     private Integer id;
 
