d270ed2f83acdae5fa57eb8048b164355853e1a0
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-03-03 11:30:38 -0600

    Se copiaron archivos de otra rama para unir todo el proyecto

---
+++ demo/src/main/java/com/inprax/demo/Service/LoginServiceImplements.java
@@ -0,0 +1,20 @@
+package com.inprax.demo.Service;
+
+import com.inprax.demo.Entity.Login;
+import com.inprax.demo.Repository.LoginRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginServiceImplements implements LoginService {
+
+    private final LoginRepository loginRepository;
+
+    public LoginServiceImplements(LoginRepository loginRepository) { this.loginRepository = loginRepository; }
+
+    @Override
+    public Login validarLogin(String correoLogin, String usuarioLogin, String contrasenaLogin, String roles) {
+        return loginRepository
+                .findByCorreoLoginAndUsuarioLoginAndContrasenaLoginAndRoles(correoLogin, usuarioLogin, contrasenaLogin, roles)
+                .orElse(null);
+    }
+}
