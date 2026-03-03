759f83b5277cacfe8792f5853e065785a38e2b6a
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-03-03 11:21:13 -0600

    Se copiaron archivos de otra rama para unir todo el proyecto

---
+++ demo/src/main/java/com/inprax/demo/Controller/EmpresaController.java
@@ -0,0 +1,70 @@
+package com.inprax.demo.Controller;
+
+import com.inprax.demo.Entity.Empresa;
+import com.inprax.demo.Service.EmpresaService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/empresas")
+public class EmpresaController {
+
+    private final EmpresaService empresaService;
+
+    public EmpresaController(EmpresaService empresaService) {
+        this.empresaService = empresaService;
+    }
+
+    @GetMapping
+    public List<Empresa> getAllEmpresas() {
+        return empresaService.getAllEmpresas();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getEmpresaById(@PathVariable @Valid Integer id) {
+        Empresa empresa = empresaService.getEmpresaById(id);
+        if (empresa != null) {
+            return ResponseEntity.ok(empresa);
+        } else {
+            return ResponseEntity.status(404).body("No existe la empresa");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createEmpresa(@Valid @RequestBody Empresa empresa) {
+        try {
+            Empresa creada = empresaService.saveEmpresa(empresa);
+            return ResponseEntity.ok().body(creada);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateEmpresa(@PathVariable Integer id, @Valid @RequestBody Empresa empresa) {
+        try {
+            Empresa actualizada = empresaService.updateEmpresa(id, empresa);
+            if (actualizada != null) {
+                return ResponseEntity.ok(actualizada);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEmpresa(@PathVariable @Valid Integer id) {
+        Empresa empresa = empresaService.getEmpresaById(id);
+        if (empresa != null) {
+            empresaService.deleteEmpresa(id);
+            return ResponseEntity.ok().body("Se elimino la empresa");
+        } else {
+            return ResponseEntity.status(404).body("No existe la empresa");
+        }
+    }
+}
