f2cd8dff31b475654a33b99e7d200e98a6acef0f
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-21 11:52:48 -0600

    feat: Se agrego la ruta para Mostrar datos y eliminar

--- demo/src/main/java/com/inprax/demo/Controller/EstudiantesController.java
+++ demo/src/main/java/com/inprax/demo/Controller/EstudiantesController.java
@@ -5,13 +5,14 @@ import com.inprax.demo.Service.EstudiantesService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/estudiantes")
+@Controller
 public class EstudiantesController {
 
     private final EstudiantesService service;
@@ -20,32 +21,18 @@ public class EstudiantesController {
         this.service = service;
     }
 
-    @GetMapping
-    public List<Estudiantes> listar() {
-        return service.getAllEstudiantes();
+    @GetMapping("/admin/estudiantes")
+    public String listarEstudiantes(Model model) {
+        List<Estudiantes> estudiantes = service.getAllEstudiantes();
+        model.addAttribute("estudiantes", estudiantes);
+        return "Index/estudiantes";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> obtener(@PathVariable Integer id) {
-        Estudiantes obtener = service.getEstudianteById(id);
-        return ResponseEntity.ok(obtener);
-    }
-
-    @PostMapping
-    public ResponseEntity<?> crear(@Valid @RequestBody Estudiantes estudiante, BindingResult result) {
-        Estudiantes nuevo = service.saveEstudiantes(estudiante);
-        return new ResponseEntity<>(nuevo, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<?> actualizar(@PathVariable Integer id, @Valid @RequestBody Estudiantes estudiante, BindingResult result) {
-        Estudiantes actualizado = service.updateEstudiantes(id, estudiante);
-        return new ResponseEntity<>(actualizado, HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletedEstudiante(@Valid @PathVariable Integer id) {
+    @GetMapping("/admin/estudiantes/eliminar/{id}")
+    public String eliminarEstudiante(@PathVariable Integer id) {
         service.deleteEstudiantes(id);
-        return ResponseEntity.ok("Estudiante eliminado exitosamente");
+        return "redirect:/admin/estudiantes";
     }
+
+
 }
