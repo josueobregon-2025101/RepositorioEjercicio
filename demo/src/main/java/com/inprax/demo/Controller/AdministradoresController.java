4a15c0d3d11faffac83fa03ed2490ad308f33426
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-21 10:47:46 -0600

    feat: Se agrego la ruta para el dashboard de admin

--- demo/src/main/java/com/inprax/demo/Controller/AdministradoresController.java
+++ demo/src/main/java/com/inprax/demo/Controller/AdministradoresController.java
@@ -1,21 +1,43 @@
 package com.inprax.demo.Controller;
 
 import com.inprax.demo.Entity.Administradores;
+import com.inprax.demo.Repository.EmpresaRepository;
+import com.inprax.demo.Repository.EstudiantesRepository;
+import com.inprax.demo.Repository.PracticasRepository;
 import com.inprax.demo.Service.AdministradoresService;
 import jakarta.validation.Valid;
+import org.springframework.ui.Model;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/administradores")
+@Controller
 public class AdministradoresController {
 
     private final AdministradoresService administradoresService;
 
     public AdministradoresController(AdministradoresService administradoresService) {
         this.administradoresService = administradoresService;
+    }
+
+    @Autowired
+    public EstudiantesRepository estudiantesRepo;
+
+    @Autowired
+    EmpresaRepository empresasRepo;
+
+    @Autowired
+    public PracticasRepository practicasRepo;
+
+    @GetMapping("/admin")
+    public String dashboard(Model model) {
+        model.addAttribute("totalEstudiantes", estudiantesRepo.count());
+        model.addAttribute("totalEmpresas", empresasRepo.count());
+        model.addAttribute("totalPracticas", practicasRepo.count());
+        return "Index/dashboard-admin";
     }
 
     @GetMapping
@@ -67,4 +89,6 @@ public class AdministradoresController {
             return ResponseEntity.status(404).body("No existe el administrador");
         }
     }
+
+    
 }
