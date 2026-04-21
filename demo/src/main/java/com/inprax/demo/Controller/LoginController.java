6c3377f3dab566c60a32745564f1087cf977149e
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-21 10:48:35 -0600

    feat: Se agrego la ruta para la pagina principal

--- demo/src/main/java/com/inprax/demo/Controller/LoginController.java
+++ demo/src/main/java/com/inprax/demo/Controller/LoginController.java
@@ -4,10 +4,10 @@ import com.inprax.demo.Entity.Login;
 import com.inprax.demo.Service.LoginService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/login")
+@Controller
 public class LoginController {
 
     private final LoginService loginService;
@@ -16,22 +16,8 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @PostMapping
-    public ResponseEntity<?> validarLogin(@Valid @RequestBody Login login) {
-        try {
-            Login resultado = loginService.validarLogin(
-                    login.getCorreoLogin(),
-                    login.getUsuarioLogin(),
-                    login.getContrasenaLogin(),
-                    login.getRoles()
-            );
-            if (resultado != null) {
-                return ResponseEntity.ok().body(resultado);
-            } else {
-                return ResponseEntity.status(401).body("Credenciales invalidas");
-            }
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    @GetMapping("/index")
+    public String index() {
+        return "Index/index";
     }
 }
