473efa27c673c40feb8db3f023ee672131b5079f
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-04-21 21:10:21 -0600

    feat:Se ha agregado el metodo para mostrar registros y se han terminado de modificar metodos HTTPs

--- demo/src/main/java/com/inprax/demo/Controller/EmpresaController.java
+++ demo/src/main/java/com/inprax/demo/Controller/EmpresaController.java
@@ -5,6 +5,7 @@ import com.inprax.demo.Service.EmpresaService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,9 +20,16 @@ public class EmpresaController {
         this.empresaService = empresaService;
     }
 
-    @GetMapping
-    public List<Empresa> getAllEmpresas() {
-        return empresaService.getAllEmpresas();
+    @GetMapping("/lista")
+    public String mostrarEmpresas(Model model) {
+        List<Empresa> listar = empresaService.getAllEmpresas();
+        model.addAttribute("empresa", listar);
+        return "Index/empresas";
+    }
+
+    @GetMapping("/empresas")
+    public String mostrarEstudiantes() {
+        return "Index/empresas";
     }
 
     @GetMapping("/{id}")
@@ -34,11 +42,6 @@ public class EmpresaController {
         }
     }
 
-    @GetMapping("/empresas")
-    public String mostrarEstudiantes() {
-        return "Index/empresas";
-    }
-
     @PostMapping
     public ResponseEntity<?> createEmpresa(@Valid @RequestBody Empresa empresa) {
         try {
@@ -49,28 +52,28 @@ public class EmpresaController {
         }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> updateEmpresa(@PathVariable Integer id, @Valid @RequestBody Empresa empresa) {
+    @PostMapping("/upgradear/{id}")
+    public String updateEmpresa(@PathVariable Integer id, @Valid @RequestBody Empresa empresa) {
         try {
             Empresa actualizada = empresaService.updateEmpresa(id, empresa);
             if (actualizada != null) {
-                return ResponseEntity.ok(actualizada);
+                return "redirect:/Index/empresas";
             } else {
-                return ResponseEntity.notFound().build();
+                return "No existe la empresa";
             }
         } catch (Exception e) {
-            return ResponseEntity.badRequest().build();
+            return e.getMessage();
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteEmpresa(@PathVariable @Valid Integer id) {
+    @GetMapping("/eliminar/{id}")
+    public String deleteEmpresa(@PathVariable @Valid Integer id) {
         Empresa empresa = empresaService.getEmpresaById(id);
         if (empresa != null) {
             empresaService.deleteEmpresa(id);
-            return ResponseEntity.ok().body("Se elimino la empresa");
+            return "redirect:/Index/empresas";
         } else {
-            return ResponseEntity.status(404).body("No existe la empresa");
+            return "No existe la empresa";
         }
     }
 }
