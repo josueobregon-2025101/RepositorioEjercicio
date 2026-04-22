1d0717d46846657cea2f386f8c7223a04d980ab3
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-22 10:14:30 -0600

    fix: Se modificaron rutas de AdminitradoresController

--- demo/src/main/java/com/inprax/demo/Controller/AdministradoresController.java
+++ demo/src/main/java/com/inprax/demo/Controller/AdministradoresController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@RequestMapping("/api/administradores")
 public class AdministradoresController {
 
     private final AdministradoresService administradoresService;
