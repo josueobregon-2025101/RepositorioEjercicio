5570298acc2f4cd3b5d9d8dd4fe282b554a5eaa5
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-22 11:06:02 -0600

    fix: Se arreglaron errores de los metodos creados para las rutas

--- demo/src/main/java/com/inprax/demo/Service/EstudiantesService.java
+++ demo/src/main/java/com/inprax/demo/Service/EstudiantesService.java
@@ -9,7 +9,6 @@ import java.util.List;
 public interface EstudiantesService {
     List<Estudiantes> getAllEstudiantes();
     Estudiantes getEstudianteById(Integer id);
-    Estudiantes saveEstudiantes (Estudiantes estudiantes) throws RuntimeException;
-    Estudiantes updateEstudiantes (Integer id, Estudiantes estudiantes);
+    void saveEstudiantes (Estudiantes estudiantes) throws RuntimeException;
     void deleteEstudiantes (Integer id);
 }
