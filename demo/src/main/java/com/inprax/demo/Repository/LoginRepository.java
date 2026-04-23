9d371075c30fc3c4b02d6756f0b2a2090bb9c6ef
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-22 21:18:14 -0600

    feat: Se agregaron nuevas cosas al LoginRepository

--- demo/src/main/java/com/inprax/demo/Repository/LoginRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/LoginRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
+
 
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Integer> {
@@ -14,5 +16,7 @@ public interface LoginRepository extends JpaRepository<Login, Integer> {
             String contrasenaLogin,
             String roles
     );
+
+    Login findByUsuarioLogin(String usuarioLogin);
 
 }
