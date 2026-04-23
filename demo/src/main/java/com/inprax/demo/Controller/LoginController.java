b831a3e90606298038005817614d4bc6dadba9e6
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-22 21:16:20 -0600

    feat: Se agregaron nuevas rutas

--- demo/src/main/java/com/inprax/demo/Controller/LoginController.java
+++ demo/src/main/java/com/inprax/demo/Controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/api/Login")
+@RequestMapping("/login")
 public class LoginController {
 
     private final LoginService loginService;
