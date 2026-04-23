d38b8dc9e4b98b12b252af27f508f0dcb80ff75e
josueobregon <jobregon-2025101@kinal.edu.gt>
2026-04-22 20:24:47 -0600

    Feat: se cambio la ruta de requestMapping de documentos

--- demo/src/main/java/com/inprax/demo/Controller/DocumentosController.java
+++ demo/src/main/java/com/inprax/demo/Controller/DocumentosController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/Documentos")
+@RequestMapping("/documentos")
 public class DocumentosController {
     private final DocumentosService documentosService;
 
