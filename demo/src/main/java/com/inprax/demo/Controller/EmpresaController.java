013073d812abe3cfc3306be64acb58e5d17bf05e
marcosmontenegro-2025031 <mmontenegro-2025031@kinal.edu.gt>
2026-04-23 22:01:12 -0600

    feat: Se agrego el metodo para conectar la tabla a la base de datos

--- demo/src/main/java/com/inprax/demo/Controller/EmpresaController.java
+++ demo/src/main/java/com/inprax/demo/Controller/EmpresaController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/empresas")
@@ -38,7 +41,9 @@ public class EmpresaController {
     }
 
     @GetMapping("/empresas")
-    public String empresas() {
+    public String empresas(Model model) {
+        List<Empresa> empresas= empresaService.getAllEmpresas();
+        model.addAttribute("empresas", empresas);
         return "Index/empresas";
     }
 
@@ -86,7 +91,7 @@ public class EmpresaController {
 
      */
 
-    @PutMapping("/{id}")
+    @PutMapping("editar/{id}")
     @ResponseBody
     public ResponseEntity<?> updateEmpresa(@PathVariable Integer id, @RequestBody @Valid Empresa empresa) {
         try {
@@ -101,19 +106,10 @@ public class EmpresaController {
         }
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity<?> deleteEmpresa(@PathVariable @Valid Integer id) {
-        Empresa empresa = empresaService.getEmpresaById(id);
-        if (empresa != null) {
-            try {
-                empresaService.deleteEmpresa(id);
-                return ResponseEntity.ok("Se elimino la Empresa " + id);
-            } catch (IllegalArgumentException e) {
-                return ResponseEntity.badRequest().body(e.getMessage());
-            }
-        } else {
-            return ResponseEntity.status(404).body("No se encontro la empresa " + id);
-        }
+    @GetMapping("/empresas/eliminar/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id) {
+        empresaService.deleteEmpresa(id);
+            return "redirect:/empresas/empresas";
     }
+    
 }
