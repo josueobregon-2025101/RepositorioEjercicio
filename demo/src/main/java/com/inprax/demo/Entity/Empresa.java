21201443fb005314410dc8b9826553669e92cf1b
Mateo <mortiz-2024204@kinal.edu.gt>
2026-04-27 22:09:32 -0600

    Fix: elimina anotaciones @NotBlank y @NotNull

--- demo/src/main/java/com/inprax/demo/Entity/Empresa.java
+++ demo/src/main/java/com/inprax/demo/Entity/Empresa.java
@@ -14,27 +14,21 @@ public class Empresa {
     @Column(name = "id_empresa")
     private Integer idEmpresa;
 
-    @NotBlank(message = "El nombre de la empresa es obligatorio")
     @Column(name = "nombre_empresa")
     private String nombreEmpresa;
 
-    @NotBlank(message = "El tipo de empresa es obligatorio")
     @Column(name = "tipo_empresa")
     private String tipoEmpresa;
 
-    @NotBlank(message = "El tamano de la empresa es obligatorio")
     @Column(name = "tamano_empresa")
     private String tamanoEmpresa;
 
-    @NotBlank(message = "El telefono de la empresa es obligatorio")
     @Column(name = "telefono_empresa")
     private String telefonoEmpresa;
 
-    @NotBlank(message = "El correo de la empresa es obligatorio")
     @Column(name = "correo_empresa")
     private String correoEmpresa;
 
-    @NotBlank(message = "La direccion de la empresa es obligatoria")
     @Column(name = "direccion_empresa")
     private String direccionEmpresa;
 
@@ -44,8 +38,6 @@ public class Empresa {
     @Column(name = "descripcion", columnDefinition = "TEXT")
     private String descripcion;
 
-    @NotNull(message = "El ID de login es obligatorio")
-    @Positive(message = "El ID de login debe ser un numero positivo")
     @Column(name = "id_login")
     private Integer idLogin;
 
