f0bcbbc73fa1d595047ecfa525ac160e30425f1f
Mateo <mortiz-2024204@kinal.edu.gt>
2026-04-27 22:04:42 -0600

    Feat: Se agrego el DTO de empresa

--- demo/src/main/java/com/inprax/demo/DTO/EmpresaDTO.java
+++ demo/src/main/java/com/inprax/demo/DTO/EmpresaDTO.java
@@ -1,4 +1,70 @@
 package com.inprax.demo.DTO;
 
 public class EmpresaDTO {
+    // LOGIN
+    private String usuarioLogin;
+    private String correoLogin;
+    private String contrasenaLogin;
+
+    // EMPRESA
+    private String nombreEmpresa;
+    private String tipoEmpresa;
+    private String telefonoEmpresa;
+    private String correoEmpresa;
+
+    public String getUsuarioLogin() {
+        return usuarioLogin;
+    }
+
+    public void setUsuarioLogin(String usuarioLogin) {
+        this.usuarioLogin = usuarioLogin;
+    }
+
+    public String getCorreoLogin() {
+        return correoLogin;
+    }
+
+    public void setCorreoLogin(String correoLogin) {
+        this.correoLogin = correoLogin;
+    }
+
+    public String getContrasenaLogin() {
+        return contrasenaLogin;
+    }
+
+    public void setContrasenaLogin(String contrasenaLogin) {
+        this.contrasenaLogin = contrasenaLogin;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getTipoEmpresa() {
+        return tipoEmpresa;
+    }
+
+    public void setTipoEmpresa(String tipoEmpresa) {
+        this.tipoEmpresa = tipoEmpresa;
+    }
+
+    public String getTelefonoEmpresa() {
+        return telefonoEmpresa;
+    }
+
+    public void setTelefonoEmpresa(String telefonoEmpresa) {
+        this.telefonoEmpresa = telefonoEmpresa;
+    }
+
+    public String getCorreoEmpresa() {
+        return correoEmpresa;
+    }
+
+    public void setCorreoEmpresa(String correoEmpresa) {
+        this.correoEmpresa = correoEmpresa;
+    }
 }
