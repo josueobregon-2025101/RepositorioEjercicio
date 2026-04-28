04b9e7058bfe3626fdeffd964b06e54d4e586031
Mateo <mortiz-2024204@kinal.edu.gt>
2026-04-27 22:11:57 -0600

    Feat: guarda login y empresa en la base de datos

--- demo/src/main/java/com/inprax/demo/Service/EmpresaServiceImplements.java
+++ demo/src/main/java/com/inprax/demo/Service/EmpresaServiceImplements.java
@@ -1,18 +1,22 @@
 package com.inprax.demo.Service;
 
 import com.inprax.demo.Entity.Empresa;
+import com.inprax.demo.Entity.Login;
 import com.inprax.demo.Repository.EmpresaRepository;
+import com.inprax.demo.Repository.LoginRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
 public class EmpresaServiceImplements implements EmpresaService {
 
     private final EmpresaRepository empresaRepository;
+    private final LoginRepository loginRepository;
 
-    public EmpresaServiceImplements(EmpresaRepository empresaRepository) {
+    public EmpresaServiceImplements(EmpresaRepository empresaRepository,
+                                    LoginRepository loginRepository) {
         this.empresaRepository = empresaRepository;
+        this.loginRepository = loginRepository;
     }
 
     @Override
@@ -26,29 +30,37 @@ public class EmpresaServiceImplements implements EmpresaService {
     }
 
     @Override
-    public Empresa saveEmpresa(Empresa empresa) {
+    public Empresa saveEmpresa(Empresa empresa, Login login) {
+        if (loginRepository.findByUsuarioLogin(login.getUsuarioLogin()) != null) {
+            return null;
+        }
+        Login loginNew = new Login();
+        loginNew.setUsuarioLogin(login.getUsuarioLogin());
+        loginNew.setCorreoLogin(login.getCorreoLogin());
+        loginNew.setContrasenaLogin(login.getContrasenaLogin());
+        loginNew.setRoles("Empresa");
+        Login loginGuardado = loginRepository.save(loginNew);
+        empresa.setIdLogin(loginGuardado.getIdLogin());
+        System.out.println("Empresa a guardar: " + empresa.getNombreEmpresa() + " | login id: " + empresa.getIdLogin());
+        System.out.println("Empresa a guardar: " + empresa.getNombreEmpresa() + " | login id: " + empresa.getIdLogin());
+        System.out.println("Tipo: " + empresa.getTipoEmpresa() + " | Correo: " + empresa.getCorreoEmpresa());
         return empresaRepository.save(empresa);
     }
 
     @Override
     public Empresa updateEmpresa(Integer id, Empresa empresa) {
-        Empresa empresaExistente = empresaRepository.findById(id).orElse(null);
-
-        if (empresaExistente == null) {
-            return null;
-        }
-
-        empresaExistente.setNombreEmpresa(empresa.getNombreEmpresa());
-        empresaExistente.setTipoEmpresa(empresa.getTipoEmpresa());
-        empresaExistente.setTamanoEmpresa(empresa.getTamanoEmpresa());
-        empresaExistente.setTelefonoEmpresa(empresa.getTelefonoEmpresa());
-        empresaExistente.setCorreoEmpresa(empresa.getCorreoEmpresa());
-        empresaExistente.setDireccionEmpresa(empresa.getDireccionEmpresa());
-        empresaExistente.setHorarioEmpresa(empresa.getHorarioEmpresa());
-        empresaExistente.setDescripcion(empresa.getDescripcion());
-        empresaExistente.setIdLogin(empresa.getIdLogin());
-
-        return empresaRepository.save(empresaExistente);
+        Empresa existente = empresaRepository.findById(id).orElse(null);
+        if (existente == null) return null;
+        existente.setNombreEmpresa(empresa.getNombreEmpresa());
+        existente.setTipoEmpresa(empresa.getTipoEmpresa());
+        existente.setTamanoEmpresa(empresa.getTamanoEmpresa());
+        existente.setTelefonoEmpresa(empresa.getTelefonoEmpresa());
+        existente.setCorreoEmpresa(empresa.getCorreoEmpresa());
+        existente.setDireccionEmpresa(empresa.getDireccionEmpresa());
+        existente.setHorarioEmpresa(empresa.getHorarioEmpresa());
+        existente.setDescripcion(empresa.getDescripcion());
+        existente.setIdLogin(empresa.getIdLogin());
+        return empresaRepository.save(existente);
     }
 
     @Override
