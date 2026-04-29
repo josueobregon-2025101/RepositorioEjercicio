b98b2f63e1bed52639687c4dd7fb1474e6b426fd
Mateo <mortiz-2024204@kinal.edu.gt>
2026-04-28 19:45:10 -0600

    Fix: cambia validarLogin por identificador+contraseña

--- demo/src/main/java/com/inprax/demo/Service/LoginService.java
+++ demo/src/main/java/com/inprax/demo/Service/LoginService.java
@@ -1,15 +1,10 @@
 package com.inprax.demo.Service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.inprax.demo.Entity.Login;
+import org.springframework.stereotype.Service;
 
 @Service
 public interface LoginService {
-
-    Login validarLogin(String correoLogin, String usuarioLogin, String contrasenaLogin, String roles);
-
+    Login validarLogin(String identificador, String contrasenaLogin);
     Login registrarLogin(String usuarioLogin, String contrasenaLogin, String correoLogin, String roles);
 }
