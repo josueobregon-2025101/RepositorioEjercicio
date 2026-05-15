4ec700354a1bafbd64353078a5910d1852892552
Mateo <mortiz-2024204@kinal.edu.gt>
2026-05-15 15:36:02 -0600

    Fix: Se corrigieron errores relacionados con entity

--- demo/src/main/java/com/inprax/demo/Repository/EstudiantesRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/EstudiantesRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EstudiantesRepository extends JpaRepository<Estudiantes, Integer> {
     boolean existsByCorreo(String correo);
-    Estudiantes findByIdLogin(Integer idlogin);
+    Estudiantes findByIdlogin(Integer idlogin);
 
 }
