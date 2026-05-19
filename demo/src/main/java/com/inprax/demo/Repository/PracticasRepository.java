3130bd627d603ce6d375709b3e2b2b779395946c
josueobregon <jobregon-2025101@kinal.edu.gt>
2026-05-18 20:50:20 -0600

    Feat: se agrego un proceso para obtener las practicas de una empresa por id

--- demo/src/main/java/com/inprax/demo/Repository/PracticasRepository.java
+++ demo/src/main/java/com/inprax/demo/Repository/PracticasRepository.java
@@ -4,7 +4,10 @@ import com.inprax.demo.Entity.Practicas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PracticasRepository extends JpaRepository <Practicas, Integer>{
+    List<Practicas> getPracticasByidEmpresa(Integer idEmpresa);
 
 }
